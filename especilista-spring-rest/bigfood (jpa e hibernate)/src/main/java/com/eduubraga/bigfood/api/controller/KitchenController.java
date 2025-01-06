f762e273a48b970571c1669b2c7f0ce2dbbfbf35
Eduardo Braga <eduardo.braga467@gmail.com>
2025-01-06 18:39:28 -0300

    Criei um método para buscar recurso único em kitchens

--- especilista-spring-rest/bigfood (jpa e hibernate)/src/main/java/com/eduubraga/bigfood/api/controller/KitchenController.java
+++ especilista-spring-rest/bigfood (jpa e hibernate)/src/main/java/com/eduubraga/bigfood/api/controller/KitchenController.java
@@ -4,6 +4,7 @@ import com.eduubraga.bigfood.domain.model.Kitchen;
 import com.eduubraga.bigfood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,10 @@ public class KitchenController {
     public List<Kitchen> list() {
         return kitchenRepository.all();
     }
+
+    @GetMapping("/{kitchenId}")
+    public Kitchen searchById(@PathVariable Long kitchenId) {
+        return kitchenRepository.byId(kitchenId);
+    }
+
 }
