c9653a7aa58ce58926bebe1f4882643772c69a7a
Eduardo Braga <eduardo.braga467@gmail.com>
2025-01-08 21:10:02 -0300

    FEAT: criei método POST para a entidade kitchen

--- especilista-spring-rest/bigfood (jpa e hibernate)/src/main/java/com/eduubraga/bigfood/api/controller/KitchenController.java
+++ especilista-spring-rest/bigfood (jpa e hibernate)/src/main/java/com/eduubraga/bigfood/api/controller/KitchenController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -31,11 +28,16 @@ public class KitchenController {
         Kitchen kitchen = kitchenRepository.byId(kitchenId);
 
         if (Objects.isNull(kitchen)) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
             return ResponseEntity.notFound().build();
         }
 
         return ResponseEntity.ok(kitchen);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Kitchen add(@RequestBody Kitchen kitchen) {
+        return kitchenRepository.add(kitchen);
     }
 
 }
