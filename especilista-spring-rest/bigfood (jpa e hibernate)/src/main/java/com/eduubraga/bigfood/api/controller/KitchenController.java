16f7bf39ecb4733c3c92eb63de199f738d34332e
Eduardo Braga <eduardo.braga467@gmail.com>
2025-01-08 21:31:41 -0300

    FEAT: Criei método PUT para a entidade kitchen

--- especilista-spring-rest/bigfood (jpa e hibernate)/src/main/java/com/eduubraga/bigfood/api/controller/KitchenController.java
+++ especilista-spring-rest/bigfood (jpa e hibernate)/src/main/java/com/eduubraga/bigfood/api/controller/KitchenController.java
@@ -2,8 +2,8 @@ package com.eduubraga.bigfood.api.controller;
 
 import com.eduubraga.bigfood.domain.model.Kitchen;
 import com.eduubraga.bigfood.domain.repository.KitchenRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -39,5 +39,21 @@ public class KitchenController {
     public Kitchen add(@RequestBody Kitchen kitchen) {
         return kitchenRepository.add(kitchen);
     }
+
+    @PutMapping("{kitchenId}")
+    public ResponseEntity<Kitchen> update(@PathVariable Long kitchenId, @RequestBody Kitchen kitchen) {
+        Kitchen currentKitchen = kitchenRepository.byId(kitchenId);
+
+        if (Objects.isNull(currentKitchen)) {
+            return ResponseEntity.notFound().build();
+        }
+
+//        currentKitchen.setName(kitchen.getName());
+        BeanUtils.copyProperties(kitchen, currentKitchen, "id");
+
+        currentKitchen = kitchenRepository.add(currentKitchen);
+
+        return ResponseEntity.ok(currentKitchen);
+    }   
 
 }
